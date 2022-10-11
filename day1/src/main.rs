15d65ff702e0720a7287f54a87de3ed83aa2cbf2
cowboykimchi <cowboykimchis@gmail.com>
2022-10-11 00:39:49 -0700

    day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, Write};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    let vec: Vec<u32> = input.lines().map(|x| x.parse().unwrap()).collect();
+    counter(&vec, 2 as usize)?;
+    counter(&vec, 4 as usize)?;
+
+    Ok(())
 }
+
+fn counter(input: &[u32], window_sz: usize) -> Result<()> {
+    let count = input.windows(window_sz).filter(|w| w[window_sz - 1] > w[0]).count();
+    writeln!(io::stdout(), "{}", count)?;
+
+    Ok(())
+}
+
